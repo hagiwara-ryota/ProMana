73194105895ac58752b7ddbe31cdeb715ca46387
ryota <otama1256@gmail.com>
2023-11-21 10:35:37 +0900

    HelloWorldと表示

---
+++ src/main/java/com/example/ProMana/ProManaApplication.java
@@ -0,0 +1,13 @@
+package com.example.ProMana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProManaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProManaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ProMana/ProManaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ProMana;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProManaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
